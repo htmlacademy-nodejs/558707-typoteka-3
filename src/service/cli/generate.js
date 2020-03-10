9fba0b3903d9dadc39fb18650364e16184ef9cd1
Nikita Korolyuk <sant_51@mail.ru>
2020-03-10 21:55:59 +0300

    Add .txt files for generate mocks

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {writeFile} = require(`fs`).promises;
+const {writeFile, readFile} = require(`fs`).promises;
 
 const {getRandomInt, shuffle, logger} = require(`../../utils`);
 const {ExitCode, Command} = require(`../../constants`);
@@ -12,6 +12,12 @@ const PublicationsCount = {
   MAX: 1000,
 };
 
+const FilePath = {
+  SENTENCES: `./data/sentences.txt`,
+  TITLES: `./data/titles.txt`,
+  CATEGORIES: `./data/categories.txt`,
+};
+
 const THREE_MONTHS_MILLISECONDS = 3 * 30 * 24 * 60 * 60 * 1000;
 
 const CURRENT_DATE = new Date();
@@ -21,55 +27,15 @@ const AnnounceSentencesCount = {
   MAX: 5,
 };
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const readContent = async (filePath) => {
+  try {
+    const content = await readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    logger.showError(err);
+    return [];
+  }
+};
 
 const addZeroForString = (number) => `${number}`.padStart(2, `0`);
 
@@ -84,19 +50,23 @@ const generateAnnounceDate = (currentDate, periodMilliseconds) => {
   return `${announceDate.getFullYear()}-${month}-${date} ${hours}:${minutes}:${seconds}`;
 };
 
-const generatePublications = (count) => (
+const generatePublications = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: generateAnnounceDate(CURRENT_DATE, THREE_MONTHS_MILLISECONDS),
-    announce: shuffle(SENTENCES).slice(0, getRandomInt(AnnounceSentencesCount.MIN, AnnounceSentencesCount.MAX)).join(` `),
-    fullText: shuffle(SENTENCES).slice(0, getRandomInt(AnnounceSentencesCount.MIN, SENTENCES.length - 1)).join(` `),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(0, CATEGORIES.length - 1)),
+    announce: shuffle(sentences).slice(0, getRandomInt(AnnounceSentencesCount.MIN, AnnounceSentencesCount.MAX)).join(` `),
+    fullText: shuffle(sentences).slice(0, getRandomInt(AnnounceSentencesCount.MIN, sentences.length - 1)).join(` `),
+    category: shuffle(categories).slice(0, getRandomInt(0, categories.length - 1)),
   }))
 );
 
 module.exports = {
   name: Command.GENERATE,
   async run(count) {
+    const sentences = await readContent(FilePath.SENTENCES);
+    const titles = await readContent(FilePath.TITLES);
+    const categories = await readContent(FilePath.CATEGORIES);
+
     const formattedCount = parseInt(count, 10) || PublicationsCount.DEFAULT;
 
     if (formattedCount > PublicationsCount.MAX) {
@@ -104,7 +74,7 @@ module.exports = {
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generatePublications(formattedCount));
+    const content = JSON.stringify(generatePublications(formattedCount, titles, categories, sentences));
 
     try {
       await writeFile(FILE_NAME, content);
