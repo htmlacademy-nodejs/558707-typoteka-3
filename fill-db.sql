3c628673bcfa0ac048e1fc19926513c07eeefa76
Nikita Korolyuk <sant_51@mail.ru>
2020-10-27 20:46:52 +0300

    Add fill-db.sql

---
+++ fill-db.sql
@@ -0,0 +1,93 @@
+INSERT INTO users VALUES
+(1,'Edison','Bruen','Danial84@gmail.com','AcYh_GeKWMNssI_','https://s3.amazonaws.com/uifaces/faces/twitter/al_li/128.jpg'),
+(2,'Milford','Harvey','Dante.Schumm@yahoo.com','xr9oZxqueYD0c2c','https://s3.amazonaws.com/uifaces/faces/twitter/mocabyte/128.jpg'),
+(3,'Clinton','Crooks','Adaline_Mann48@gmail.com','QlCchneQibk6Xjw','https://s3.amazonaws.com/uifaces/faces/twitter/low_res/128.jpg'),
+(4,'Marion','Pfannerstill','Sonya21@gmail.com','VI86yDzTFIwarFR','https://s3.amazonaws.com/uifaces/faces/twitter/sydlawrence/128.jpg'),
+(5,'Kennedi','Boyer','Judge.Dibbert@yahoo.com','YE2LLmHCVrm07jg','https://s3.amazonaws.com/uifaces/faces/twitter/exentrich/128.jpg');
+
+INSERT INTO articles VALUES
+(1,'Rerum ipsam deserunt sit delectus nobis quae occaecati vel.','Et ratione eaque voluptates assumenda doloribus numquam est omnis. Aspernatur harum blanditiis error aut. Et consequatur facilis.',Sun Jul 12 2020 19:42:52 GMT+0300 (GMT+03:00),'eos magni ad','http://placeimg.com/640/480/business'),
+(2,'Delectus eius delectus voluptatibus dolores aut et molestiae.','Recusandae blanditiis magni porro. Nihil error delectus nulla eos ipsa ex dicta voluptate voluptatum. Repellat in sed.',Tue Jan 28 2020 06:08:36 GMT+0300 (GMT+03:00),'magnam recusandae voluptas','http://placeimg.com/640/480/people'),
+(3,'Vel occaecati molestias doloremque magni ipsum quasi.','Iste aperiam quas laborum. Placeat incidunt cupiditate explicabo fuga quod.',Tue Dec 17 2019 22:00:13 GMT+0300 (GMT+03:00),'quas exercitationem quos','http://placeimg.com/640/480/sports'),
+(4,'Nihil adipisci beatae.','Dolorem distinctio aut itaque rerum quae qui quasi. Nostrum animi laudantium deleniti enim dolorem exercitationem corrupti. Et cumque quia dolorum aut et fuga labore quasi. Doloremque error suscipit maiores quisquam praesentium debitis et voluptatibus vel. Minima deserunt et laboriosam odio beatae quo neque eum necessitatibus. Expedita illum reprehenderit ut expedita.',Mon Apr 27 2020 14:26:39 GMT+0300 (GMT+03:00),'non','http://placeimg.com/640/480/fashion'),
+(5,'At et necessitatibus et dolore praesentium labore cum molestiae.','Aut dolorem explicabo. Quia non cum ullam et sunt fugiat quasi. Molestiae ipsam nulla deleniti vel enim assumenda. In animi voluptatem officia debitis aut perferendis est.',Fri Mar 20 2020 13:17:17 GMT+0300 (GMT+03:00),'Totam esse architecto. Tenetur dolores nulla labore dolor dolorem. Qui aliquid ipsa debitis aspernatur sint.','http://placeimg.com/640/480/cats'),
+(6,'Molestias placeat ratione fugit ut non non quia cumque mollitia.','Voluptas perferendis similique ratione in. Qui repellat qui inventore est incidunt ipsam eius. Nihil qui ut vel aliquam consequuntur. Odio modi omnis velit dignissimos quidem quod placeat. Nihil et quo et ipsa temporibus magni voluptatem.',Fri Aug 14 2020 18:06:58 GMT+0300 (GMT+03:00),'Quo beatae neque qui fuga qui consequatur nesciunt laudantium id. Et natus voluptatem aut nisi ut laborum pariatur. Ad alias possimus et quam corporis. Voluptatem assumenda voluptatibus occaecati sequi exercitationem omnis eum. Dolor eos qui dolor. Et nesciunt ex.','http://placeimg.com/640/480/animals'),
+(7,'Expedita ab quae ut velit asperiores debitis.','Natus ut ea dolores nihil debitis est amet magni ipsam. Nesciunt et doloribus dolorum sed neque unde repudiandae eveniet. Quia a recusandae voluptatum soluta voluptas id doloremque deleniti. Velit explicabo consequuntur placeat commodi corporis qui. Qui similique odio commodi pariatur minus qui enim dolore.',Mon Sep 14 2020 20:46:33 GMT+0300 (GMT+03:00),'Eos eveniet sit sequi voluptates ut praesentium. Accusamus assumenda rerum enim nostrum blanditiis asperiores. Voluptatum nam rerum provident maxime iste aut ipsum. Eum neque ipsum velit similique.','http://placeimg.com/640/480/animals'),
+(8,'Dolor nihil nostrum itaque impedit soluta.','Rerum cum nihil sunt culpa. Qui aliquid beatae eius error commodi nihil. Quos sint quis inventore.',Thu Oct 15 2020 02:50:19 GMT+0300 (GMT+03:00),'sunt doloribus doloribus','http://placeimg.com/640/480/food'),
+(9,'Odio delectus molestiae voluptatem consequuntur excepturi.','Fugit mollitia et doloribus. Qui quibusdam ut. Minus omnis aut voluptas qui omnis velit quo. Eos rerum aut tempore quae eos dolorem.',Sat Jan 18 2020 15:37:24 GMT+0300 (GMT+03:00),'Ea nihil ab.','http://placeimg.com/640/480/people'),
+(10,'Veritatis voluptatibus eum molestias dolorem laudantium.','Voluptas quia soluta. Nulla at ut temporibus. Et possimus dolores et ut natus.',Tue Sep 22 2020 07:22:45 GMT+0300 (GMT+03:00),'Aliquid fugiat aliquid consectetur necessitatibus. Et quo voluptas molestiae itaque nisi est ut. Sunt dolorem aut sit temporibus iusto sed. Deleniti incidunt sunt voluptas eos exercitationem est qui earum et. Et quia perspiciatis atque maxime repellat vel eius eum. Doloremque et corrupti qui sint fugiat sed itaque magnam sit.','http://placeimg.com/640/480/business');
+
+INSERT INTO categories VALUES
+(1,'Деревья'),
+(2,'За жизнь'),
+(3,'Без рамки'),
+(4,'Разное'),
+(5,'IT'),
+(6,'Музыка'),
+(7,'Кино'),
+(8,'Программирование'),
+(9,'Железо'),
+(10,'Учение'),
+(11,'Природа');
+
+INSERT INTO comments VALUES
+(1,'Fugit necessitatibus est commodi esse asperiores libero. Voluptate doloremque qui nam dolores repellendus non velit. Nemo quam accusantium. Autem voluptas cum qui eaque mollitia vel quaerat. Sit excepturi illum ex neque aut accusantium consequuntur hic.',5,9),
+(2,'Sit dolorum quaerat rem quae ratione velit. Odio soluta officiis ullam ipsum. Est molestias facere assumenda eveniet. Quia omnis minus sunt id.',3,10),
+(3,'Aut quia non deserunt quia minus in iusto. Veritatis aut et.',4,9),
+(4,'Sit qui autem dolore. Consequatur molestiae voluptas commodi accusamus nobis molestiae iste ratione et. Repudiandae nihil molestiae et quisquam consequuntur. Eligendi enim fugit aperiam quae error.',3,5),
+(5,'Quia ratione esse itaque voluptates totam tenetur repellat. Inventore culpa quia qui.',4,1),
+(6,'Tempore accusamus aperiam quibusdam voluptatem nostrum autem. Hic est recusandae tempore facilis vitae quia adipisci.',1,10),
+(7,'Ut atque et. Voluptatem possimus quod. Autem vel eum nulla reiciendis expedita sit magnam recusandae. Neque nulla quasi consequatur expedita nemo quaerat eum.',4,5),
+(8,'Facilis repellendus qui quibusdam voluptates adipisci sed reprehenderit eius. Eos pariatur quia voluptatem vel alias eveniet qui libero. Repellat voluptas sed dolorem at quo a suscipit architecto. Eveniet non architecto alias non aliquam praesentium quae vel.',2,8),
+(9,'Odio cumque voluptatem rerum. Consequatur ut a iusto dolorem provident qui ipsa labore quia. Dolores architecto dicta odit enim. Labore est accusamus quia ullam enim. Vel itaque quia eos inventore nam dicta atque.',4,4),
+(10,'Inventore tempore nesciunt eius odit. Et fugiat dolorem qui porro quisquam libero mollitia quam inventore. Dolorem consequuntur quis tempora voluptas ullam.',1,3),
+(11,'Maxime voluptatibus id exercitationem sint doloremque quo temporibus impedit in. Necessitatibus voluptatibus veniam pariatur iure. Quis quos eum sunt.',5,10),
+(12,'Et libero qui eaque. Quasi ea unde quia exercitationem molestiae corrupti cupiditate id quam. Aut ut eum debitis nemo aut qui adipisci modi harum.',1,3),
+(13,'Qui qui assumenda mollitia mollitia qui ab. Cumque voluptate quidem.',2,9),
+(14,'Optio quibusdam voluptate. Harum enim optio omnis inventore voluptates est. Omnis illum ex et architecto molestiae illum culpa. Voluptas doloremque enim voluptate non cum consequatur est.',3,3),
+(15,'Veritatis error modi hic. Non aut ut dolor aut. Odio itaque est est fugiat quo est ut. Quia et eaque fuga nihil ipsam quae earum. Tempora nostrum qui adipisci praesentium laboriosam architecto. Ipsa reprehenderit odio repudiandae laborum.',1,9);
+
+INSERT INTO users_articles VALUES
+(1,1),
+(1,2),
+(5,3),
+(5,4),
+(2,5),
+(4,6),
+(1,7),
+(1,8),
+(1,9),
+(2,10);
+
+INSERT INTO articles_categories VALUES
+(1,1),
+(2,2),
+(3,3),
+(1,4),
+(2,5),
+(3,6),
+(1,7),
+(2,8),
+(3,9),
+(1,10),
+(2,11),
+(3,1),
+(1,2),
+(2,3),
+(3,4),
+(1,5),
+(2,6),
+(3,7),
+(1,8),
+(2,9),
+(3,10),
+(1,11),
+(2,1),
+(3,2),
+(1,3),
+(2,4),
+(3,5),
+(1,6),
+(2,7),
+(3,8);
+
