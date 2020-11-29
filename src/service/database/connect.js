979364af687c59085392a3625a6112484b05f837
Nikita Korolyuk <sant_51@mail.ru>
2020-11-29 19:28:58 +0300

    Add db connect

---
+++ src/service/database/connect.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Sequelize = require(`sequelize`);
+const pinoLogger = require(`../../pino-logger`);
+
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env;
+
+(async () => {
+  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
+
+  pinoLogger.info(`Connection started`);
+
+  try {
+    await sequelize.authenticate();
+    pinoLogger.info(`Connection is successful`);
+  } catch (err) {
+    pinoLogger.error(`Error: ${err}`);
+  }
+})();
